45ced56b6c302027c7124e7af75d44ea83ac143a
kandelarjun67 <kandelarjun67@gmail.com>
2023-04-26 23:11:05 +0545

    rename the file register to index.js

--- client/src/pages/register/index.js
+++ client/src/pages/register/index.js
@@ -1,11 +1,11 @@
 const Register = () => {
     return (
-        <>
+        <div>
         <input placeholder="Full Name"/>
         <input placeholder="Mobile Number"/>
         <input placeholder="Password"/>
         <button>Submit</button>
-        </>
+        </div>
     )
 }
 export default Register
